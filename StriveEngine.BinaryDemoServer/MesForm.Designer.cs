838edc2da2067179cd24f28c80e107bf09cdac60
524300045@qq.com <524300045@qq.com>
2018-11-08 15:12:01 +0800

    1111

--- StriveEngine.BinaryDemoServer/MesForm.Designer.cs
+++ StriveEngine.BinaryDemoServer/MesForm.Designer.cs
@@ -28,33 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCom = new System.Windows.Forms.ComboBox();
+            this.rtbReceive = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbRate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbContent = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnOpen
             // 
-            this.button1.Location = new System.Drawing.Point(100, 66);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnOpen.Location = new System.Drawing.Point(489, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "打开串口";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(489, 50);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "关闭串口";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "串口号:";
+            // 
+            // cbCom
+            // 
+            this.cbCom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCom.FormattingEnabled = true;
+            this.cbCom.Location = new System.Drawing.Point(100, 26);
+            this.cbCom.Name = "cbCom";
+            this.cbCom.Size = new System.Drawing.Size(121, 20);
+            this.cbCom.TabIndex = 3;
+            // 
+            // rtbReceive
+            // 
+            this.rtbReceive.Location = new System.Drawing.Point(58, 206);
+            this.rtbReceive.Name = "rtbReceive";
+            this.rtbReceive.Size = new System.Drawing.Size(427, 96);
+            this.rtbReceive.TabIndex = 4;
+            this.rtbReceive.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "接受数据";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "波特率:";
+            // 
+            // tbRate
+            // 
+            this.tbRate.Location = new System.Drawing.Point(290, 25);
+            this.tbRate.Name = "tbRate";
+            this.tbRate.Size = new System.Drawing.Size(126, 21);
+            this.tbRate.TabIndex = 6;
+            this.tbRate.Text = "9600";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "发送数据:";
+            // 
+            // tbContent
+            // 
+            this.tbContent.Location = new System.Drawing.Point(103, 82);
+            this.tbContent.Name = "tbContent";
+            this.tbContent.Size = new System.Drawing.Size(243, 21);
+            this.tbContent.TabIndex = 7;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(374, 82);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "发送数据";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // MesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 276);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(595, 341);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbContent);
+            this.Controls.Add(this.tbRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtbReceive);
+            this.Controls.Add(this.cbCom);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
             this.Name = "MesForm";
             this.Text = "MesForm";
+            this.Load += new System.EventHandler(this.MesForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCom;
+        private System.Windows.Forms.RichTextBox rtbReceive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbRate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbContent;
+        private System.Windows.Forms.Button btnSend;
     }
 }
--- StriveEngine.BinaryDemoServer/MesForm.cs
+++ StriveEngine.BinaryDemoServer/MesForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,19 +12,144 @@ namespace StriveEngine.BinaryDemoServer
 {
     public partial class MesForm : Form
     {
+
+        //   SerialPort serialPort1 = new SerialPort("COM2", 9600, Parity.None, 8, StopBits.One);      //初始化串口设置
+
+        SerialPort serialPort1;
+        public delegate void DisplaydelegateOne(byte[] InputBuf);
+
+        public DisplaydelegateOne disp_delegate;
+
         public MesForm()
         {
             InitializeComponent();
         }
 
-        /// <summary>
-        /// 调用实例
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button1_Click(object sender, EventArgs e)
+
+
+        private void MesForm_Load(object sender, EventArgs e)
         {
+            string[] ports = SerialPort.GetPortNames();
+            this.cbCom.Items.AddRange(ports);
+            if (ports != null && ports.Count() > 0)
+            {
+                this.cbCom.SelectedIndex = 0;
+            }
+
+            disp_delegate = new DisplaydelegateOne(DispUI);
+          
+        }
+
+        public void DispUI(byte[] InputBuf)
+        {
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string info = encoding.GetString(InputBuf);
+
+            rtbReceive.Text += info + "\r\n";
 
         }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (serialPort1 == null)
+            {
+                serialPort1 = new SerialPort(this.cbCom.Text, int.Parse(tbRate.Text.Trim()), Parity.Even, 7, StopBits.One);
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(Comm_DataReceived);
+            }
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.Open();
+                }
+
+                this.btnClose.Enabled = true;
+                this.btnOpen.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        void Comm_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            SerialPort sp = (SerialPort)sender;
+
+            Byte[] InputBuf = new Byte[128];
+
+            try
+            {
+                serialPort1.Read(InputBuf, 0, serialPort1.BytesToRead);                                //读取缓冲区的数据直到“}”即0x7D为结束符
+                //InputBuf = UnicodeEncoding.Default.GetBytes(strRD);             //将得到的数据转换成byte的格式
+                System.Threading.Thread.Sleep(50);
+                this.Invoke(disp_delegate, InputBuf);
+
+            }
+            catch (TimeoutException ex)         //超时处理
+            {
+                LogHelper.Log("Comm_DataReceived:" + ex.Message);
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //if (string.IsNullOrEmpty(tbContent.Text.Trim()))
+            //{
+            //    MessageBox.Show("请输入发送内容");
+            //    return;
+            //}
+            try
+            {
+                string result = HexConvert.Hex2Ten(tbContent.Text.Trim());
+                //serialPort1.Write(result);
+                byte[] bData = new byte[3];
+                //bData[0] = 0x16;
+                //bData[1] = 0x54;
+                //bData[2] = 0x0D;
+
+                bData[0] = 22;
+                bData[1] =84;
+                bData[2] =13;
+
+                serialPort1.Write(bData,0,3);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+           // string result = HexConvert.Hex2Ten(tbContent.Text.Trim());
+
+         
+
+         //   byte[] bb = strToHexByte(tbContent.Text.Trim());
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+            this.btnClose.Enabled = false;
+            this.btnOpen.Enabled = true;
+        }
+
+        private static byte[] strToHexByte(string hexString)
+        {
+            hexString = hexString.Replace(" ", "");
+            if ((hexString.Length % 2) != 0)
+                hexString += " ";
+            byte[] returnBytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < returnBytes.Length; i++)
+                returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            return returnBytes;
+        }
+
     }
 }
